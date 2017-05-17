ae6bd64b88c8f26c6b08ad1e679b2043c3d98191
sunyton <sunytoncxw@gmail.com>
2017-05-18 00:38:14 +0800

    Initial commit

---
+++ app/src/main/java/com/codelearn/suny/v2exx/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codelearn.suny.v2exx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
